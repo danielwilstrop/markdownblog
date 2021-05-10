931b20e63b9f1f50fb6f087a8df046ac21838a48
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-05-10 20:46:16 +0100

    minor change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
+const cors = require('cors')
 require('dotenv').config()
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
@@ -14,12 +15,20 @@ mongoose.connect(process.env.MONGO_DB, {
 })
 
 app.set('view engine', 'ejs')
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
+
+app.use(express.static(__dirname + 'views'))
 
 app.get('/', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' })
   res.render('articles/index', { articles: articles })
+})
+
+app.get('/allposts', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.json({ articles })
 })
 
 app.use('/articles', articleRouter)
